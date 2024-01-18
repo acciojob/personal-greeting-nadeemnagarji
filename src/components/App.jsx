682a266f3f4ab7d51baacaf1a228f31c8f691589
Nadeem <nadeemnagarji@gmail.com>
2024-01-18 15:31:54 +0530

    added code in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import Input from "./Input";
+
 const App = () => {
+
+  const [data,setData] = useState("")
+
+  let handleInput = (e)=>{
+      if(e.target.value !==""){
+          let message = `Hello ${e.target.value}!`
+          setData(message)
+      }else{
+          setData("")
+      }
+
+  }
+
   return (
     <div>
-        <Input />
+     <p>Enter your name:</p>
+    <input type="text" onChange={handleInput}/>
+    {data? <p>{data}</p> :null}
 
     </div>
   )
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
 export default function Input() {
 
-    const [data,setData] = useState("")
-
-    let handleInput = (e)=>{
-        if(e.target.value !==""){
-            let message = `Hello ${e.target.value}!`
-            setData(message)
-        }else{
-            setData("")
-        }
-
-    }
+   
 
    return(
 
 
     <>
-    <p>Enter your name:</p>
-    <input type="text" onChange={handleInput}/>
-    {data? <p>{data}</p> :null}
+ 
     </>
    )
 };
